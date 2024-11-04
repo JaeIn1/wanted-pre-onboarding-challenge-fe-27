3356e92274107bc98681d7c5d6a92561e928f2a9
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-11-04 21:32:09 +0900

    feat : 로그인 구현 완료

---
+++ frontend/onboarding/src/page/Login/Login.style.ts
@@ -0,0 +1,88 @@
+import styled from "styled-components";
+
+export const LoginLayout = styled.div`
+  height: calc(100vh - ${({ theme }) => theme.navbarHeight});
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LoginDiv = styled.div`
+  width: 400px;
+  padding: 20px;
+  box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.15);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  & h3 {
+    margin-bottom: 20px;
+  }
+  & input {
+    padding: 10px 20px;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    border-radius: 30px;
+    margin-bottom: 20px;
+
+    &:focus {
+      outline: 1px solid black;
+    }
+
+    &::placeholder {
+      color: rgba(0, 0, 0, 0.4);
+    }
+  }
+`;
+export const LoginDivTop = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  margin-bottom: 30px;
+`;
+
+export const LoginButton = styled.button`
+  border: none;
+  border-radius: 30px;
+  background-color: black;
+  color: white;
+  padding: 13px 20px;
+  margin: 5px;
+`;
+
+export const LoginForm = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  margin-bottom: 30px;
+`;
+
+export const LoginFormDiv = styled.div`
+  width: 100%;
+  margin-bottom: 20px;
+
+  & input {
+    width: 100%;
+    padding: 10px 20px;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    border-radius: 30px;
+    margin-bottom: 10px;
+
+    &:focus {
+      outline: 1px solid black;
+    }
+
+    &::placeholder {
+      color: rgba(0, 0, 0, 0.4);
+    }
+  }
+`;
+
+export const ErrorText = styled.p`
+  color: tomato;
+  font-size: 0.8rem;
+  margin-left: 10px;
+`;
---
+++ frontend/onboarding/src/page/Login/Login.tsx
@@ -0,0 +1,84 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import NavbarPage from "../../components/Navbar/Navbar";
+import * as S from "./Login.style";
+import { loginUser } from "../../apis/user";
+import { AxiosError } from "axios";
+import { User } from "../../types/user";
+
+const LoginPage = () => {
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<User>();
+
+  const onClickLogin: SubmitHandler<User> = async (data) => {
+    try {
+      const userData = {
+        email: data.email,
+        password: data.password,
+      };
+      const res = await loginUser(userData);
+      const token = res.data.token;
+      localStorage.setItem("token", token);
+
+      navigate("/todo");
+    } catch (error) {
+      const axiosError = error as AxiosError;
+      if (axiosError.response) {
+        alert(axiosError.response.data.details);
+      }
+    }
+  };
+
+  const onClickSignup = () => {
+    navigate("/signup");
+  };
+
+  return (
+    <>
+      <NavbarPage />
+      <S.LoginLayout>
+        <S.LoginDiv>
+          <S.LoginDivTop>
+            <h3>Login</h3>
+            <S.LoginForm onSubmit={handleSubmit(onClickLogin)}>
+              <S.LoginFormDiv>
+                <input
+                  type="text"
+                  placeholder="email"
+                  {...register("email", {
+                    required: "이메일은 필수 입력입니다.",
+                  })}
+                />
+                {errors.email && (
+                  <S.ErrorText>{errors.email.message}</S.ErrorText>
+                )}
+              </S.LoginFormDiv>
+              <S.LoginFormDiv>
+                <input
+                  type="password"
+                  placeholder="password"
+                  {...register("password", {
+                    required: "패스워드는 필수 입력입니다.",
+                  })}
+                />
+                {errors.password && (
+                  <S.ErrorText>{errors.password.message}</S.ErrorText>
+                )}
+              </S.LoginFormDiv>
+              <S.LoginButton type="submit">Login</S.LoginButton>
+              <S.LoginButton type="button" onClick={onClickSignup}>
+                Signup
+              </S.LoginButton>
+            </S.LoginForm>
+          </S.LoginDivTop>
+        </S.LoginDiv>
+      </S.LoginLayout>
+    </>
+  );
+};
+
+export default LoginPage;
